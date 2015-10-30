59fd32b487c01e9985939a75ccf6b3f8498516c2
Denis Mahmúd <denis.mahmud@gmail.com>
2015-10-30 17:29:29 +0100

    Building a RESTful Web Service

---
+++ building_a_RESTful_Web_Service/src/main/java/maven/spring/building_a_restful_web_service/Application.java
@@ -0,0 +1,13 @@
+package maven.spring.building_a_restful_web_service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
