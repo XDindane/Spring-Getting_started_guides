82a4bb18c1df3da660e56ba1132a9ed4347acd10
Denis Mahmúd <denis.mahmud@gmail.com>
2015-11-19 18:11:45 +0100

    Creating batch service

---
+++ Creating_batch_service/src/main/java/io/spring/creating_batch_service/BatchConfiguration.java
@@ -0,0 +1,111 @@
+package io.spring.creating_batch_service;
+
+import javax.sql.DataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@EnableBatchProcessing //adds many critical beans that support jobs and saves you a lot of leg work. This example uses a memory-based database (provided by @EnableBatchProcessing), meaning that when it’s done, the data is gone.
+public class BatchConfiguration {
+
+    //tag::readerwriterprocessor[]
+    /**
+     * The first chunk of code defines the input, processor, and output. -
+     * reader() creates an ItemReader. It looks for a file called
+     * sample-data.csv and parses each line item with enough information to turn
+     * it into a Person. - processor() creates an instance of our
+     * PersonItemProcessor you defined earlier, meant to uppercase the data.
+     */
+    @Bean
+    public ItemReader<Person> reader() {
+        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
+        reader.setResource(new ClassPathResource("sample-data.csv"));
+        reader.setLineMapper(new DefaultLineMapper<Person>() {
+            {
+                setLineTokenizer(new DelimitedLineTokenizer() {
+                    {
+                        setNames(new String[]{"firstName", "lastName"});
+                    }
+                });
+                setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {
+                    {
+                        setTargetType(Person.class);
+                    }
+                });
+            }
+        });
+        return reader;
+    }
+
+    @Bean
+    public ItemProcessor<Person, Person> processor() {
+        return new PersonItemProcessor();
+    }
+
+    /**
+     *
+     * his one is aimed at a JDBC destination and automatically gets a copy of
+     * the dataSource created by @EnableBatchProcessing. It includes the SQL
+     * statement needed to insert a single Person driven by Java bean
+     * properties.
+     *
+     * @param dataSource
+     * @return
+     */
+    @Bean
+    public ItemWriter<Person> writer(DataSource dataSource) {
+        JdbcBatchItemWriter<Person> writer = new JdbcBatchItemWriter<Person>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Person>());
+        writer.setSql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)");
+        writer.setDataSource(dataSource);
+        return writer;
+    }
+    // end::readerwriterprocessor[]
+    // tag::jobstep[] 
+
+    @Bean
+    public Job importUserJob(JobBuilderFactory jobs, Step s1, JobExecutionListener listener) {
+        return jobs.get("importUserJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(s1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Person> reader,
+            ItemWriter<Person> writer, ItemProcessor<Person, Person> processor) {
+        return stepBuilderFactory.get("step1")
+                .<Person, Person>chunk(10)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+    // end::jobstep[]
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+}
