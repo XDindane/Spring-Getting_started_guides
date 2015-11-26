c76a5e9bf9fdabf74bf84026aafe5d35f764c937
Denis Mahmúd <denis.mahmud@gmail.com>
2015-11-26 17:21:55 +0100

    Accessing data with JPA

---
+++ gs-accessing-data-jpa/initial/src/main/java/hello/Customer.java
@@ -0,0 +1,30 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Customer() {
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer[id=%d, firstname='%s', lastName='%s']", id, firstName, lastName);
+    }
+
+}
